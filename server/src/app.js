7cf1a58b77080151efb3cc59b610009c96db47dc
Robby <ta7382@gmail.com>
2017-09-04 17:23:55 +0800

    update qrcode to server

--- server/src/app.js
+++ server/src/app.js
@@ -37,8 +37,8 @@ app
     }))
     .use(routes())
 
-app.listen(3000,
-    () => console.log(`✅  The server is running at http://localhost:3000/`)
+app.listen(80,
+    () => console.log(`✅  The server is running at http://localhost:80/`)
 )
 
 export default app
